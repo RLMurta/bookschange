adf5fe9b45fa0fd71d76e8275b4782a7a49b2bef
RLMurta <rafaellmurta@hotmail.com>
2021-09-06 15:23:28 -0300

    Implementando BooksRepository.kt e arquivo de Exceptions

--- app/src/main/java/com/compasso/bookschange/model/home/BookApi/BooksRepository.kt
+++ app/src/main/java/com/compasso/bookschange/model/home/BookApi/BooksRepository.kt
@@ -1,5 +1,18 @@
 package com.compasso.bookschange.model.home.BookApi
 
 class BooksRepository (private val api: ApiInterface){
+    suspend fun fetch(searchTerms: String) : List<BooksResponse> {
+        val response = api.fetch(searchTerms)
+        if(response.isSuccessful){
+            return response.body()!!
+        } else if(response.code() == BOOK_NOT_FOUND){
+            throw BookNotFoundException()
+        }else {
+            throw Exception()
+        }
+    }
 
+    companion object{
+        private const val BOOK_NOT_FOUND = 404
+    }
 }
---
+++ app/src/main/java/com/compasso/bookschange/model/home/BookApi/Exceptions.kt
@@ -0,0 +1,5 @@
+package com.compasso.bookschange.model.home.BookApi
+
+import java.lang.Exception
+
+class BookNotFoundException : Exception()
